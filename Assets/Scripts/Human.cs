6d2e2c504a4eed429083dfdd9a020568717b1191
celiue <celine.de.wijs@gmail.com>
2019-07-06 17:25:27 +0200

    Made the AI chase and catch the player and then go back to its start position.

--- Assets/Scripts/Human.cs
+++ Assets/Scripts/Human.cs
@@ -4,23 +4,31 @@ using UnityEngine;
 using Panda;
 
 public class Human : MonoBehaviour {
-    
-    public float walkSpeed = 3.5f;
-    public float jumpSpeed = 5.5f;
-    public Transform player;
+
+    public GameObject player;
+    public Transform startPos;
+
+    private float walkSpeed = 2.25f;
+    private float jumpSpeed = 5.5f;
+    private Transform target;
+    public int hugPlayerTime;
 
     private Vector3[] path;
     private int targetIndex;
     private Vector3 destination;
     private List<Human> humans = new List<Human>();
+    private bool finishedPath = true;
 
     void Start() {
-        player = FindObjectOfType<Player>().transform;
-        PathRequestManager.RequestPath(transform.position, player.position, OnPathFound);
+        target = player.transform;
+        transform.position = startPos.position;
+       // PathRequestManager.RequestPath(transform.position, player.position, OnPathFound);
     }
 
+    #region Pathfinding
     public void OnPathFound(Vector3[] newPath, bool pathSuccessful) {
         if (pathSuccessful) {
+            Debug.Log("Path is succesful");
             path = newPath;
             StopCoroutine("FollowPath");
             StartCoroutine("FollowPath");
@@ -30,63 +38,91 @@ public class Human : MonoBehaviour {
     private IEnumerator FollowPath() {
         Vector3 currentWaypoint = path[0];
 
+        Debug.Log("Current waypoint: " + currentWaypoint);
+        Debug.Log("Position: " + transform.position);
+        Debug.Log("Player position: " + target.position);
+        
         while (true) {
             if (transform.position == currentWaypoint) {
                 targetIndex++;
                 if (targetIndex >= path.Length) {
+                    finishedPath = true;
                     yield break;
                 }
                 currentWaypoint = path[targetIndex];
             }
-            transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, walkSpeed);
+            transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, walkSpeed / 10);
             yield return null;
         }
     }
 
-    #region Tasks
+    #endregion
 
+    #region Tasks
     [Task]
     bool IsPlayerNear = false;
 
     [Task]
+    bool IsChasingTarget = false;
+
+    [Task]
     void CheckPlayer() {
-        float distanceToPlayer = Vector3.Distance(player.position, transform.position);
-        if (distanceToPlayer < 8.0f) {
+        
+        float distanceToPlayer = Vector3.Distance(target.position, transform.position);
+        if (distanceToPlayer < 15.0f) {
             IsPlayerNear = true;
             Task.current.Succeed();
         } else {
             IsPlayerNear = false;
             Task.current.Fail();
         }
-        Task.current.Succeed();
     }
 
     [Task] 
-    void SetDestinationToPlayer() {
-        destination = player.transform.position;
-        Task.current.Succeed();
-    }
-
-    [Task] 
-    void MoveToDestination() {
-        Vector3 distance = destination - transform.position;
-
-        if (transform.position != destination) {
-            Vector3 velocity = distance.normalized * walkSpeed;
-            transform.position = transform.position + velocity * Time.deltaTime;
-
-            Vector3 newDistance = destination - transform.position;
-            if (Vector3.Dot(newDistance, distance) < 0.0f) {
-                transform.position = destination;
-            }
-        }
-
-        float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
-        if (distanceToPlayer < 2.0f) {
+    void RequestPathAndGoToTarget() {
+        if (finishedPath) {
+            IsChasingTarget = true;
+            finishedPath = false;
+            PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
             Task.current.Succeed();
-        } else {
-            Task.current.Fail();
         }
     }
+
+    [Task]
+    void CheckIfReachedTarget() {
+        float distanceToTarget = Vector3.Distance(target.position, transform.position);
+        Debug.Log(target);
+        if (distanceToTarget < 2.0f) {
+            IsChasingTarget = false;
+            finishedPath = true;
+            Task.current.Succeed();
+        } 
+    }
+
+    [Task]
+    void HoldPlayer() {
+        player.GetComponent<PlayerMovement>().FreezeControls(true);
+        Task.current.Succeed();
+    }
+
+    [Task]
+    void ReleasePlayer() {
+        player.GetComponent<PlayerMovement>().FreezeControls(false);
+        Task.current.Succeed();
+    }
+
+    [Task]
+    void GoToStartPosition() {
+        target = startPos;
+        Debug.Log("Target = " + target);
+
+        if (finishedPath) {
+            IsChasingTarget = true;
+            finishedPath = false;
+            PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
+            Task.current.Succeed();
+        }
+    }
+
     #endregion
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,10 +1,14 @@
 ﻿using UnityEngine;
 
-
 public class PlayerMovement : MonoBehaviour {
-    CharacterController characterController;
+
     public float movementSpeed = 5.0f;
+    
+    private CharacterController characterController;
     private Vector3 moveDirection = Vector3.zero;
+
+    private float lastMovementSpeed;
+
     void Start() {
         characterController = GetComponent<CharacterController>();
     }
@@ -16,5 +20,14 @@ public class PlayerMovement : MonoBehaviour {
         //Gravity
         moveDirection.y -= 10f * Time.deltaTime;
         characterController.Move(moveDirection * Time.deltaTime);
+    }
+
+    public void FreezeControls(bool freeze) {
+        if (freeze) {
+            lastMovementSpeed = movementSpeed;
+            movementSpeed = 0;
+        } else {
+            movementSpeed = lastMovementSpeed;
+        }
     }
 }
